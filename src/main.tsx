3d3f040418f568a8d20186b34b046d1c137c1f04
Erik Maung <infiniteerik123@gmail.com>
2025-02-28 01:54:58 -0800

    put something out

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <div>
+      <h1>Chromatic Quest</h1>
+      <App />
+    </div>
 ,
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,15 +5,15 @@ import Scene from '../components/Scene';
 import DynamicBackground from '../components/DynamicBackground';
 
 const Home: React.FC = () => {
-  const [backgroundColor, setBackgroundColor] = useState('#ffffff');
+  const [backgroundColor, setBackgroundColor] = useState('#ef135f20');
 
   const handleColorChange = (color: string) => {
-    setBackgroundColor(color);
+    setBackgroundColor(color + '20');
   };
 
   return (
     <div>
-      <h1>Chromatic Quest: The Interactive Color Theory Adventure</h1>
+      <h1>Nothing here yet, development in progress.</h1>
       <ColorWheel onColorChange={handleColorChange} />
       <Scene />
       <DynamicBackground color={backgroundColor} />
